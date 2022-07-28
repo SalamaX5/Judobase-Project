fc4c368acc1fbddf5e40838bc84b7c6941652fcb
SalamaX5 <104665358+SalamaX5@users.noreply.github.com>
2022-07-28 17:13:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,11 @@ def get_competitions():
     start_dates = []
     end_dates = []
     countries = []
+    comp_id = []
     while start_comp <= end_comp:
         driver.get(f'https://judobase.ijf.org/#/competition/profile/{start_comp}/statistics')
         try:
-            WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="well"]')))
+            WebDriverWait(driver, 1).until(EC.element_to_be_clickable((By.XPATH, '//div[@class="well"]')))
         except TimeoutException:
             start_comp += 1
             continue
@@ -47,15 +48,26 @@ def get_competitions():
             start_dates.append(total_list[1])
             end_dates.append(total_list[2])
             countries.append(total_list[3])
+            comp_id.append(start_comp)
         start_comp += 1
 
-    competitions = pd.DataFrame(np.column_stack([comps, countries, start_dates, end_dates]),
-                                columns=['Competition', 'Host Country', 'Start Date', 'End Date'])
+    competitions = pd.DataFrame(np.column_stack([comp_id, comps, countries, start_dates, end_dates]),
+                                columns=['comp_id', 'competition', 'host_country', 'start_date', 'end_date'])
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
+    competitions.set_index('comp_id', inplace=True)
+
+    discard = ["Kata"]
+    competitions = competitions[~competitions.competition.str.contains('|'.join(discard))]
+    competitions['start_date'] = pd.to_datetime(competitions['start_date'], format='%Y-%m-%d')
+    competitions = competitions.loc[(competitions['start_date'] > '2016-09-22')]
+
     print(competitions)
+
+    #  to save as CSV file
+    #  competitions.to_csv('competitions.csv')
 
 
 get_competitions()
